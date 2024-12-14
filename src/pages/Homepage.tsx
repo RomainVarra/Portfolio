8586bda51fc66902ec4ee326245d25f9de49ffa4
Romain Varra <romain.varra@hotmail.com>
2024-12-14 20:48:37 +0100

    feat: creation of animations pictures for the homePage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,10 +1,14 @@
 import Contact from "../components/Contact";
+import ImagesSlider from "../components/ImagesSilder";
 import MyProjects from "../components/MyProjects";
+import leftImage from "../../public/Images/7c28c-josef1.jpg"
+import rightImage from "../../public/Images/Capture d'écran 2024-09-22 182220.png"
 
 function Homepage () {
     return(
         <>
         <h1>Hello Portfolio</h1>
+        <ImagesSlider leftImage = {leftImage} rightImage= {rightImage}/>
         <MyProjects/>
         <Contact/>
         </>
