ab42cee4a636251ea47b6cc5d9550f957b369561
Romain Varra <romain.varra@hotmail.com>
2024-12-17 00:20:25 +0100

    feat: creation of the burger menu

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,30 +1,42 @@
 import { useTranslation } from "../../contexts/LangContext";
 import style from "./NavBar.module.css"
 import { NavLink, useNavigate } from "react-router-dom";
+import { useState } from "react";
 function NavBar() {
     const navigate = useNavigate();
     const handleClick = () => {
         navigate("/");
       };
     const {language, toggleLanguage, translations} = useTranslation();
+    const [menu, setMenu] = useState(false);
+    const burgerMenu = () => setMenu(!menu);
+    const booleanMenu = () => setMenu(false);
     return(
         
         <section className={style.navBarStyle}>
         <img className={style.myLogo} src="Images/logoPortfolio.webp" alt="Logo du site"
         onClick={handleClick}
          />
-        <ul className={style.navLink}>
+         <button
+        className={style.burgerButton}
+        onClick={burgerMenu}
+      >
+        <span className={style.burgerBar}></span>
+        <span className={style.burgerBar}></span>
+        <span className={style.burgerBar}></span>
+      </button>
+        <ul className={`${style.navLink} ${menu ? style.showMenu : style.hideMenu}`}>
             <li>
-            <NavLink to={"/"}>{translations.NavBar.home}</NavLink>
+            <NavLink to={"/"} onClick={booleanMenu}>{translations.NavBar.home}</NavLink>
             </li>
             <li>
-            <NavLink to={"/about"}>{translations.NavBar.about}</NavLink>
+            <NavLink to={"/about"} onClick={booleanMenu}>{translations.NavBar.about}</NavLink>
             </li>
             <li>
-            <NavLink to={"/project"}>{translations.NavBar.projects}</NavLink>
+            <NavLink to={"/project"} onClick={booleanMenu}>{translations.NavBar.projects}</NavLink>
             </li>
             <li>
-            <NavLink to={"/contact"}>{translations.NavBar.contact}</NavLink>
+            <NavLink to={"/contact"} onClick={booleanMenu}>{translations.NavBar.contact}</NavLink>
             </li>
            <li>
   <button onClick={toggleLanguage} className={style.buttonLanguage} >
