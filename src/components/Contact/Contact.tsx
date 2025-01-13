a35271cc9bda8aa1bd85980a91befcc9f581b74d
Romain Varra <romain.varra@hotmail.com>
2025-01-13 19:41:24 +0100

    feat: transformation of contact section

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,24 +1,34 @@
-import { useTranslation } from "../../contexts/LangContext"
-import style from "./contact.module.css"
+import { useTranslation } from "../../contexts/LangContext";
+import style from "./contact.module.css";
+import {
+    FaFacebook,
+    FaLinkedin,
+    FaEnvelope,
+    FaGithub
+  } from "react-icons/fa";
 
 function Contact() {
     const {translations} = useTranslation();
     return(
         <>
         <h1 className={style.Title}>{translations.NavBar.contact}</h1>
-        <section className={style.contactSection}>
+        <article className={style.contactSection}>
             <p className={style.contactText}>{translations.Contact.Contact_me}</p>
-            <form className={style.contactForm} action="">
-                <label htmlFor="">
-                    <input className={style.contactFormName} type="text" placeholder="Name" />
-                </label>
-                <label htmlFor=""><input className={style.contactFormEmail} type="text" placeholder="Email" /></label>
-                <label htmlFor=""><input className={style.contactFormText} type="text" placeholder="Message" /></label>
-            </form>
+        <section className={style.iconeContactSection}>
+        <a href="mailto:romain.varra@hotmail.com" target="_blank" rel="noreferrer">
+        <FaEnvelope className={style.contactIcone} /> 
+        </a>
+            <a href="https://www.linkedin.com/in/romain-varra/" target="_blank" rel="noreferrer">
+            <FaLinkedin className={style.contactIcone} />
+          </a>
+          <a href="https://github.com/RomainVarra/" target="_blank" rel="noreferrer">
+            <FaGithub className={style.contactIcone} />
+          </a>
+          <a href="https://www.facebook.com/romain.varra" target="_blank" rel="noreferrer">
+            <FaFacebook className={style.contactIcone}/>
+          </a>   
         </section>
-        <section className={style.contactFormSubmitSection}>
-            <button className={style.contactFormSubmit} type="submit">{translations.Contact.submit}</button>
-        </section>
+        </article>
         </>
     )
 }
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -12,11 +12,11 @@ function Footer() {
         <p>© Portfolio Romain Varra</p>
             </section>
             <section >
-            <a className={style.footerIcone} href="https://www.linkedin.com/in/romain-varra/" target="_blank" rel="noreferrer">
-            <FaLinkedin />
+            <a href="https://www.linkedin.com/in/romain-varra/" target="_blank" rel="noreferrer">
+            <FaLinkedin className={style.footerIcone} />
           </a>
-          <a className={style.footerIcone} href="https://www.facebook.com/romain.varra" target="_blank" rel="noreferrer">
-            <FaFacebook />
+          <a href="https://www.facebook.com/romain.varra" target="_blank" rel="noreferrer">
+            <FaFacebook className={style.footerIcone} />
           </a>        
             </section>
         </article>
