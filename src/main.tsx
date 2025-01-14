28ccf16b21719fe0f213705a1f047650a61a773b
Romain Varra <romain.varra@hotmail.com>
2025-01-14 19:04:58 +0100

    feat: fix some style before deployement

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './index.css'
 
 
 
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router = {router}/>
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,12 +2,13 @@ import ImagesSlider from "../components/ImagesSlider/ImagesSilder";
 import leftImage from "../../public/Images/photoProfilPersonnalisé.jpg"
 import rightImage from "../../public/Images/PhotoProfil.jpg"
 import { useTranslation } from "../contexts/LangContext";
+import style from "../components/NavBar/NavBar.module.css"
 
 function Homepage () {
     const {translations} = useTranslation();
     return(
         <>
-        <h1>{translations.Homepage.maintitle}</h1>
+        <h1 className={style.welcomeTitle}>{translations.Homepage.maintitle}</h1>
         <ImagesSlider leftImage = {leftImage} rightImage= {rightImage}/>
         </>
     )
