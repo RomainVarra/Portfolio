2188ee7ec246de23713172974a396e7afc96e640
Romain Varra <romain.varra@hotmail.com>
2025-04-26 16:35:03 +0200

    feat: created navLink between latestWork and projectsDetails

--- src/components/LatestWork/LatestWork.tsx
+++ src/components/LatestWork/LatestWork.tsx
@@ -2,9 +2,12 @@ import style from "./latestWork.module.css"
 import projects from "../../data/Projects.json"
 import { useEffect, useState } from "react";
 import type { ProjectsType } from "../../lib/types";
+import { useNavigate } from "react-router-dom";
 
 function LatestWork(){
 const [latestProjects, setLatestProjects] = useState<ProjectsType[]>([]);
+const navigate = useNavigate();
+const handleClick = (id: number) => navigate(`/projects/${id}`);
 
 useEffect(() => {
     const sortedProjects = [...projects].sort((a, b) => b.id - a.id);
@@ -17,7 +20,7 @@ useEffect(() => {
         <h2 className={style.mainTitleLatestWork}>Latest</h2>
         <section className={style.latestWorkSection}>
         {latestProjects?.map((p) => (
-            <article className={style.latestWorkCard} key={p.id}>
+            <article className={style.latestWorkCard} key={p.id} onClick={() => handleClick(p.id)}>
             <img className={style.latestWorkImg} src={p.picture} alt={p.name} />
             <p className={style.latestWorkName}>{p.name}</p>
             </article>
